31d52db56e5f3421255fd8eab3076a750c187b2c
Antarpreet <pnarain82@gmail.com>
2023-07-20 20:47:08 +1000

    javascript

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 var task_list = [];
 var entryList = [];
 var badList = [];
+let taskListElm = document.getElementById("good-task");
+let badtaskListElm = document.getElementById("bad-task");
 const handleOnSubmit = (e) => {
   const form = new FormData(e);
   const task = form.get("item");
   const time = form.get("time");
   const obj = { task, time, type: "entry", id: randomString() };
   task_list.push(obj);
-  display(entryList);
-  console.log(obj);
+  display();
 };
-let taskListElm = document.getElementById("good-task");
-let badtaskListElm = document.getElementById("bad-task");
+
 const display = () => {
   let str = "";
 
@@ -22,11 +22,13 @@ const display = () => {
 <td>${item.task}</td>
 <td>${item.time}</td>
 <td class="text-end">
-  <button onclick="handleOnDelete(${i})" class="btn btn-danger"><i class="fa-solid fa-trash-can"></i></i>
-  </button>
-  <button onclick="handleOnSwitch(${
+  <button onclick="handleOnDelete('${
     item.id
-  }, 'bad')" class="btn btn-success"><i class="fa-solid fa-arrow-right"></i></i></i>
+  }')" class="btn btn-danger"><i class="fa-solid fa-trash-can"></i></i>
+  </button>
+  <button onclick="handleOnSwitch('${
+    item.id
+  }', 'bad')" class="btn btn-success"><i class="fa-solid fa-arrow-right"></i></i></i>
   </button>
 </td>
 </tr>`;
@@ -42,11 +44,13 @@ const displayBad = () => {
   <td>${item.task}</td>
   <td>${item.time}</td>
   <td class="text-end">
-  <button onclick="handleOnSwitch( ${
+  <button onclick="handleOnSwitch( '${
     item.id
-  }, 'entry')" class="btn btn-warning"><i class="fa-solid fa-arrow-left"></i></i></i>
+  }', 'entry')" class="btn btn-warning"><i class="fa-solid fa-arrow-left"></i></i></i>
   </button>
-    <button onclick="handleOnDelete(${i})" class="btn btn-danger"><i class="fa-solid fa-trash-can"></i></i>
+    <button onclick="handleOnDelete('${
+      item.id
+    }')" class="btn btn-danger"><i class="fa-solid fa-trash-can"></i></i>
     </button>
     
   </td>
@@ -54,19 +58,30 @@ const displayBad = () => {
   });
   badtaskListElm.innerHTML = str;
 };
-const handleOnDelete = (index) => {
+const handleOnDelete = (id) => {
   if (window.confirm("Are you sure you want to delete this task")) {
-    task_list.splice(index, 1);
-    display(task_list);
-    displayBad();
-  }
-};
-const handleOnSwitch = (i, type) => {
-  if (window.confirm("Are you sure you want to move this task")) {
-    task_list[i].type = type;
+    task_list = task_list.filter((item) => item.id !== id);
     display();
     displayBad();
   }
 };
-const ranStr = ["qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"];
-const randomString = () => {};
+const handleOnSwitch = (id, type) => {
+  if (window.confirm("Are you sure you want to move this task")) {
+    task_list.forEach((item) => {
+      if (item.id == id) {
+        item.type = type;
+      }
+    });
+    display();
+    displayBad();
+  }
+};
+const ranStr = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+const randomString = () => {
+  const strlength = 6;
+  let str = "";
+  for (let i = 0; i < strlength; i++) {
+    str += ranStr[Math.floor(Math.random() * ranStr.length)];
+  }
+  return str;
+};
